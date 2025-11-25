5cd320cd3e0f44401a563618bd3235191722671d
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-11-25 23:51:13 +0600

    interface

--- src/app/modules/notification/notification.interface.ts
+++ src/app/modules/notification/notification.interface.ts
@@ -3,4 +3,10 @@ import { Types } from "mongoose";
 export interface INotification {
   _id?: Types.ObjectId;
   user: Types.ObjectId; // receiver of the notification
+  title: string;
+  message: string;
+  type: string;
+  seen: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
