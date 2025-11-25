1f5ef782b94ae4446afd647a23a3b3a1144357d8
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-11-25 23:52:01 +0600

    TransactionTypeEnum

--- src/app/modules/notification/notification.interface.ts
+++ src/app/modules/notification/notification.interface.ts
@@ -1,10 +1,11 @@
 import { Types } from "mongoose";
+import { TransactionTypeEnum } from "../transaction/transaction.interface";
 
 export interface INotification {
   _id?: Types.ObjectId;
   user: Types.ObjectId; // receiver of the notification
   title: string;
-  message: string;
+  message: TransactionTypeEnum;
   type: string;
   seen: boolean;
   createdAt?: Date;
