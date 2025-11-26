decac3c7d59d487a5579e4d4c1254b1b48d2ef8f
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-11-26 18:50:56 +0600

    get my notification

--- src/app/modules/notification/notificaiton.route.ts
+++ src/app/modules/notification/notificaiton.route.ts
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Notification } from "./notification.model";
+import { JwtPayload } from "jsonwebtoken";
 
 const router = Router();
+
+router.get("/my-notifications", checkAuth(), async (req, res) => {
+  const userId = (req.user as JwtPayload).userId;
+  const notifications = await Notification.find({ user: userId })
+    .sort({ createdAt: -1 })
+    .limit(50);
+  res.json(notifications);
+});
 
 export const NotificationRoutes = router;
